d52194ca7c54205e92cf678495d9ac104bbb853e
0rishav <rishavrajraj1155@gmail.com>
2024-04-12 11:38:54 +0530

    cors issue fixed

--- constants/config.js
+++ constants/config.js
@@ -1,8 +1,13 @@
+import dotenv from "dotenv";
+dotenv.config();
 const corsOptions = {
+ 
     origin:["http://localhost:5173","http://localhost:4173",process.env.CLIENT_URL],
     methods:["GET","POST","PUT","DELETE"],
     credentials:true
   }
+
+  
 
 const CHAT_APP = "ChatApp-Token"
 
